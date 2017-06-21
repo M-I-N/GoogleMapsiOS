3e5f6e9c8aac0c718a06ef23503fd629026d5380
Nayem BJIT <nayem@Mac-mini.local>
2017-06-21 14:10:19 +0600

    Updated code for MapView loading in view. Put multiple markers by reading location from json file

--- GMapsDemo/ViewController.m
+++ GMapsDemo/ViewController.m
@@ -11,50 +11,78 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) GMSMapView *mapView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    CLLocationCoordinate2D cameraPosition = [self positionForCameraSetup];
+    [self loadGMapsInViewWithLatitude:cameraPosition.latitude logitude:cameraPosition.longitude andZoomLevel:13];
+    [self putMarkersInMapView];
+
 }
 
-- (GMSMapView *)loadGMapsInView:(UIView *)view withLatitude:(CLLocationDegrees)latitude logitude:(CLLocationDegrees)longitude andZoomLevel:(float)zoom {
+- (void)loadGMapsInViewWithLatitude:(CLLocationDegrees)latitude logitude:(CLLocationDegrees)longitude andZoomLevel:(float)zoom {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.808114, 151.211825 at zoom level 6.
-      GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:latitude
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:latitude
                                                             longitude:longitude
                                                                  zoom:zoom];
-    GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView.myLocationEnabled = YES;
-    view = mapView;
-    return mapView;
+    self.mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    self.mapView.myLocationEnabled = YES;
+    self.view = self.mapView;
+
 }
 
-- (void)loadView {
-    
-    GMSMapView *commonMapView = [self loadGMapsInView:self.view withLatitude:33.808114 logitude:151.211825 andZoomLevel:6];
-    
-    // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.808114, 151.211825);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
-    marker.map = commonMapView;
-    
-    GMSMarker *marker2 = [[GMSMarker alloc] init];
-    marker2.position = CLLocationCoordinate2DMake(-32.865032, 151.785861);
-    marker2.title = @"Newcastle";
-    marker2.snippet = @"Australia";
-    marker2.map = commonMapView;
-    
+- (void)putMarkersInMapView {
+
+    NSDictionary *locationInfoDictionary = [self JSONFromFile];
+    NSArray *locationInfos = [locationInfoDictionary valueForKey:@"LocationInfo"];
+    for (NSDictionary *each in locationInfos) {
+        
+        NSString *latitudeString = each[@"Latitude"];
+        NSString *longitudeString = each[@"Longitude"];
+        CLLocationDegrees latitude = latitudeString.doubleValue;
+        CLLocationDegrees longitude = longitudeString.doubleValue;
+        
+        // Creates a marker in the center of the map.
+        GMSMarker *marker = [[GMSMarker alloc] init];
+        marker.position = CLLocationCoordinate2DMake(latitude, longitude);
+//        marker.title = each[@"Id"];
+        marker.snippet = each[@"Address"];
+        marker.appearAnimation = kGMSMarkerAnimationPop;
+        marker.map = self.mapView;
+    }
 }
 
+- (NSDictionary *)JSONFromFile
+{
+    NSDataAsset *locationAsset = [[NSDataAsset alloc]initWithName:@"locationData" bundle:[NSBundle mainBundle]];
+    
+    return [NSJSONSerialization JSONObjectWithData:locationAsset.data options:kNilOptions error:nil];
+}
 
+- (CLLocationCoordinate2D)positionForCameraSetup {
+    NSDictionary *locationInfoDictionary = [self JSONFromFile];
+    NSArray *locationInfos = [locationInfoDictionary valueForKey:@"LocationInfo"];
+    NSUInteger count = [locationInfos count];
+    NSUInteger midposition = (count - 1)/2;
+    NSDictionary *middleLocation = [locationInfos objectAtIndex:midposition-1];
+    NSString *latitudeString = middleLocation[@"Latitude"];
+    NSString *longitudeString = middleLocation[@"Longitude"];
+    CLLocationDegrees latitude = latitudeString.doubleValue;
+    CLLocationDegrees longitude = longitudeString.doubleValue;
+    
+    CLLocationCoordinate2D position = CLLocationCoordinate2DMake(latitude, longitude);
+    return position;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
